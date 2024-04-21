a0bbc946817ce24455ed8be876272ad7bb93df22
Buhle-Masondo <buhlemm22@gmail.com>
2024-04-21 23:35:11 +0200

    apiKey fixed

--- src/index.js
+++ src/index.js
@@ -11,10 +11,10 @@ function search(event) {
   let searchInputElement = document.querySelector("#search-input");
   let city = searchInputElement.value;
 
-  let apiKey = "b2a5adcct04b33178913oc335f405433";
+  let apiKey = "9350b84ao1ec86e0ba4t6f9791f5dabb";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
 
-  axios.get(apiUrl).then(displayTemperature);
+  axios.get(apiUsrl).then(displayTemperature);
 }
 
 function formatDate(date) {
